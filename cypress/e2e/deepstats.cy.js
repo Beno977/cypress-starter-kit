49a479f900ca3751d72b948fd32a4b124e2f6c51
beno.atanesyan <beniaminatanesyan@gmail.com>
2024-11-08 16:57:19 +0400

    d

--- cypress/e2e/deepstats.cy.js
+++ cypress/e2e/deepstats.cy.js
@@ -7,5 +7,15 @@ describe('My First Test', () => {
       cy.get('.players-header > button').click();
       //cy.get('.close-popup').click();
       cy.get('.popup-container .min-table-main-row:first-child').click();
-    })
+      cy.get('.search-container').click();
+      cy.get('input').type('Artur');
+      //cy.get('.search-r-tabs').click();
+      cy.get('.search-r-tabs p[_ngcontent-serverApp-c4]:last-child').click();
+      cy.get('.search-inner-wrapper > div:nth-child(2)').click();
+      cy.get('.open-close-bl').click();
+      cy.get('.open-close-bl').click();
+      cy.get('.personal-rec-info:first-child').click();
+      cy.get('.ref-link').trigger('mouseover');
+      cy.get('.min-table-col main:first-child').click();
+       })
   });
